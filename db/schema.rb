5e4519e7dc9f90ddee267453b173d7bd223b9b7c
maria_lobillo <maria.lobillo.santos@gmail.com>
2015-10-11 08:33:06 +0200

    add user_id to post: migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151011062945) do
+ActiveRecord::Schema.define(version: 20151011063210) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -19,7 +19,10 @@ ActiveRecord::Schema.define(version: 20151011062945) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
   end
+
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
