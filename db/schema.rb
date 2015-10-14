803a71c8bf8d32e9130c680fe37388accb3a5f1e
maria_lobillo <maria.lobillo.santos@gmail.com>
2015-10-14 20:07:12 +0200

    edit devise registration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151011063210) do
+ActiveRecord::Schema.define(version: 20151014180250) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20151011063210) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
