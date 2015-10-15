67d5202306933e1a059d390443800410223347f7
maria_lobillo <maria.lobillo.santos@gmail.com>
2015-10-15 18:23:54 +0200

    add image to posts table by paperclip

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151014180250) do
+ActiveRecord::Schema.define(version: 20151015162259) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.string   "link"
     t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
     t.integer  "user_id"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   add_index "posts", ["user_id"], name: "index_posts_on_user_id"
